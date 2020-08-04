4ae984c0d0efab98fe871f289a0847c8803ae518
melodc <melodc@uw.edu>
2020-08-03 20:35:30 -0700

    Settings page with new card design, Can edit project title

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/styles';
-import { Typography, Button, Card } from '@material-ui/core';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Button, Card, IconButton} from '@material-ui/core';
 import Page from 'src/components/Page';
 import Grid from '@material-ui/core/grid';
-import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { shadows } from '@material-ui/system';
+import EditIcon from '@material-ui/icons/Edit';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CloseIcon from '@material-ui/icons/Close';
+import firebase from '../../firebase/firebase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,6 +32,9 @@ const useStyles = makeStyles(theme => ({
     },
     horizontalAlign: 'left'
   },
+  content: {
+    padding: theme.spacing(5)
+  },
   header: {
     margin: theme.spacing(3),
     display: 'flex',
@@ -42,24 +45,28 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3)
   },
   grid: {
-    flexGrow: 1
+    display: 'flex',
+    flexGrow: 0,
+    paddingBottom: theme.spacing(3),
+  },
+  title: {
+    display: 'flex', 
+    width: '200px',
+    justifyContent: 'left',
+    alignItems: 'center'
+
   },
   subtitle: {
-    marginTop: theme.spacing(3),
-    paddingLeft: theme.spacing(3),
-    paddingBottom: theme.spacing(3)
+    display: 'flex',
+    justifyContent: 'left',
+    alignItems: 'center'
   },
-  divider: {
-    height: theme.spacing(1)
+  dialog: {
+    width: '100%'
   },
-  formControl: {
-    margin: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-    minWidth: 120
-  },
-  member: {
-    marginTop: theme.spacing(2),
-    paddingLeft: theme.spacing(4)
+  dialogTitle: {
+    marginTop: theme.spacing(1),
+    paddingBottom: 0
   }
 }));
 
@@ -67,126 +74,216 @@ function Settings(props) {
   const classes = useStyles();
   const session = useSelector(state => state.session);
   const [userType, setUserType] = React.useState('');
+  const [newTitle, setTitle] = useState(session.currentProject.title);
+
+  const [hasEdited, setHasEdited] = useState(false);
+
+  const [open, setOpen] = React.useState(false);
 
   const handleChange = event => {
     setUserType(event.target.value);
   };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+
+
+  const CustomTypography = withStyles(theme => ({
+    h3: {
+      color: '#05486E'
+    }
+  }))(Typography);
+
+  const handleProjectTitle = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleNewTitle = () => {
+    firebase.db
+      .collection('projects')
+      .doc(session.currentProject.id)
+      .update({
+        title: newTitle,
+      })
+      .then(() => window.location.reload())
+      .catch(function(error) {
+        console.error('Error renaming project: ', error);
+      });
+    setHasEdited(true);
+    handleClose();
+  };
+
   return (
     <Page title={'Settings'}>
-      <List
-        component="nav"
-        className={classes.root}
-        aria-label="mailbox folders"
-      >
-        <ListItem boxShadow={3}>
-          <Typography variant="h5" className={classes.title}>
-            Project Owner{' '}
-          </Typography>
-        </ListItem>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          {session.currentProject.owner}
-        </Typography>
-        <Divider className={classes.divider} />
+      <Card className={classes.root}>
+        <CardContent className={classes.content}>
+          <Grid
+            container
+            justify="flex-start"
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3} className={classes.title}>
+              <Typography variant="h5" >
+                Project Title{' '}
+              </Typography>
+            </Grid>
+            <Grid item className={classes.subtitle}>
+              <Typography variant="subtitle1">
+              {!hasEdited ? session.currentProject.title : newTitle}
+            </Typography>
+            </Grid>
 
-        <ListItem boxShadow={3}>
-          <Typography variant="h5">Members </Typography>
-        </ListItem>
-        <Grid
-          container
-          direction={'column'}
-          className={classes.grid}
-          spacing={2}
-        >
-          {/* {session.currentProject.members.managers.map(manager => (
-                    <Grid container direction={'row'} className={classes.grid} spacing={2}>
-                        <Grid item xs={3}>
-                        <Typography variant="subtitle1" className={classes.member}>{manager}</Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                            <FormControl variant="outlined" className={classes.formControl}>
-                            <InputLabel id="demo-simple-select-outlined-label">User Type</InputLabel>
-                                <Select
-                                labelId="demo-simple-select-outlined-label"
-                                id="demo-simple-select-outlined"
-                                onChange={handleChange}
-                                defaultValue="Manager"
-                                label="User Type"
-                                >
-                                <MenuItem selected value="Manager">Manager</MenuItem>
-                                <MenuItem value="Collaborator">Collaborator</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                ))} */}
+            <Grid item xs>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+              <Button
+              onClick={handleClickOpen}
+              >
+                
+               <EditIcon />
+              </Button>
+              
+            </Typography>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" className={classes.dialog}>
+              <DialogTitle className={classes.dialogTitle}>
+                  <Grid container spacing={20}>
+                <Grid item md={6} xs={12}>
+                  <CustomTypography align="left" gutterBottom variant="h3">
+                    Create Project
+                  </CustomTypography>
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <IconButton
+                    className={classes.closeIcon}
+                    onClick={handleClose}
+                    aria-label="close"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Change the title of the project.
+                  </DialogContentText>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="title"
+                    label="New Project Title"
+                    placeholder="Enter new Project title"
+                    onChange={handleProjectTitle}
+                    fullWidth
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button onClick={handleNewTitle} color="primary">
+                    Change Title
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </Grid>
 
-          {/* {session.currentProject.members.collaborators.map(collaborator => (
-                    <Grid container direction={'row'} className={classes.grid} spacing={2}>
 
-                        <Grid item xs={3}>
-                        <Typography variant="subtitle1" className={classes.member}>{collaborator}</Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                            <FormControl variant="outlined" className={classes.formControl}>
-                            <InputLabel id="demo-simple-select-outlined-label">User Type</InputLabel>
-                                <Select
-                                labelId="demo-simple-select-outlined-label"
-                                id="demo-simple-select-outlined"
-                                defaultValue="Collaborator"
-                                onChange={handleChange}
-                                label="User Type"
-                                >
+          </Grid>
 
-                                <MenuItem value="Manager">Manager</MenuItem>
-                                <MenuItem selected value="Collaborator">Collaborator</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                ))}
-                 */}
-        </Grid>
+          <Grid
+            container
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3}  className={classes.title}>
+              <Typography variant="h5">
+                Project Owner{' '}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.subtitle}>
+              <Typography variant="subtitle1" >
+              {session.currentProject.owner}
+            </Typography>
+            </Grid>
+          </Grid>
 
-        <Divider className={classes.divider} />
+          <Grid
+            container
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3}  className={classes.title}>
+              <Typography variant="h5">
+                Project ID
+              </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.subtitle}>
+              <Typography variant="subtitle1" >
+               {session.currentProject.id}
+            </Typography>
+            </Grid>
+          </Grid>
 
-        <ListItem boxShadow={3}>
-          <Typography variant="h5">Start Date </Typography>
-        </ListItem>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          {session.currentProject.start_date}
-        </Typography>
+          <Grid
+            container
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3} className={classes.title}>
+              <Typography variant="h5" >
+                Project Status{' '}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.subtitle}>
+              <Typography variant="subtitle1" >
+              {session.currentProject.status}
+            </Typography>
+            </Grid>
+          </Grid>
 
-        <Divider className={classes.divider} />
+          <Grid
+            container
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3} className={classes.title}>
+              <Typography variant="h5" >
+                Project Type{' '}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.subtitle}>
+              <Typography variant="subtitle1" >
+              {session.currentProject.type}
+            </Typography>
+            </Grid>
+          </Grid>
 
-        <ListItem boxShadow={3}>
-          <Typography variant="h5">Projected Completion Date </Typography>
-        </ListItem>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          {session.currentProject.end_date}
-        </Typography>
-        <Divider className={classes.divider} />
-
-        <ListItem boxShadow={3}>
-          <Typography variant="h5">Project Type </Typography>
-        </ListItem>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          {session.currentProject.type}
-        </Typography>
-
-        <Divider className={classes.divider} />
-
-        <ListItem boxShadow={3}>
-          <Typography variant="h5">Project Status </Typography>
-        </ListItem>
-        <Typography variant="subtitle1" className={classes.subtitle}>
-          {session.currentProject.status}
-        </Typography>
-        <Divider className={classes.divider} />
-
-        <Button variant="outlined" color="primary" className={classes.button}>
-          Save Changes
-        </Button>
-      </List>
+          <Grid
+            container
+            className={classes.grid}
+            spacing={2}
+          >
+            <Grid item xs={3} className={classes.title}>
+              <Typography variant="h5" >
+                Linked Aquarium Folder{' '}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} className={classes.subtitle}>
+              <Typography variant="subtitle1" >
+              {session.currentProject.folder}
+            </Typography>
+            </Grid>
+          </Grid>
+        
+        </CardContent>
+      </Card>
+      
     </Page>
   );
 }
