54d0a91c632a8dfb9530c27d6192d3ef40ebe6cc
lekhacminhphuong <leanutella192@gmail.com>
2020-08-04 19:50:51 -0700

    Cleaned up plan table file

--- src/components/Plans/PlanTable.js
+++ src/components/Plans/PlanTable.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import moment from 'moment';
 import Page from 'src/components/Page';
 import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
   LinearProgress,
   Button
 } from '@material-ui/core';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import StarIcon from '@material-ui/icons/Star';
 import LinkIcon from '@material-ui/icons/Link';
-import SearchBar from 'src/components/SearchBar';
 import MUIDataTable from "mui-datatables";
 
 const columns = [
@@ -82,35 +73,11 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const StyledTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white
-  }
-}))(TableCell);
-
 export default function PlanTable(props) {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(6);
   const [favStatus, setFavStatus] = React.useState(false);
-
-  const handleSearch = () => { };
-
-  const handleFilter = () => { };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  const handleFavorite = () => {
-    setFavStatus(!favStatus);
-  };
 
   useEffect(() => { }, [props.data]);
 
@@ -160,119 +127,6 @@ export default function PlanTable(props) {
         data={data}
         options={options}
       />
-      {/* 
-      <SearchBar onFilter={handleFilter} onSearch={handleSearch} className={classes.searchBar} />
-      <Paper className={classes.root}>
-        <TableContainer className={classes.container}>
-          <Table sticky Header>
-            <TableHead>
-              <TableRow>
-                {columns.map(column => (
-                  <StyledTableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </StyledTableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {props.data == null ? (
-                <LinearProgress className={classes.progress} color="primary" />
-              ) : (
-                  props.data
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map(row => {
-                      return (
-                        <TableRow
-                          hover
-                          role="checkbox"
-                          tabIndex={-1}
-                          key={JSON.parse(row.data).id}
-                        >
-                          {columns.map(column => {
-                            const value = JSON.parse(row.data)[column.id];
-                            if (column.id === 'favorite') {
-                              if (favStatus) {
-                                return (
-                                  <TableCell key={column.id} align={column.align}>
-                                    <StarIcon
-                                      style={{
-                                        color: '#065683',
-                                        cursor: 'pointer'
-                                      }}
-                                      onClick={handleFavorite}
-                                    />
-                                  </TableCell>
-                                );
-                              } else {
-                                return (
-                                  <TableCell key={column.id} align={column.align}>
-                                    <StarBorderIcon
-                                      style={{
-                                        color: '#065683',
-                                        cursor: 'pointer'
-                                      }}
-                                      onClick={handleFavorite}
-                                    />
-                                  </TableCell>
-                                );
-                              }
-                            } else if (
-                              column.id === 'created_at' ||
-                              column.id === 'updated_at'
-                            ) {
-                              return (
-                                <TableCell key={column.id} align={column.align}>
-                                  {moment(value).format('D MMM YYYY')}
-                                </TableCell>
-                              );
-                            } else if (column.id == 'id') {
-                              return (
-                                <TableCell key={column.id} align={column.align}>
-                                  <Button
-                                    href={
-                                      'http://52.27.43.242/launcher?plan_id=' +
-                                      value
-                                    }
-                                    className={classes.linkButton}
-                                    target="_blank"
-                                  >
-                                    {`${value.toFixed(0)}   `} <LinkIcon />
-                                  </Button>
-                                </TableCell>
-                              );
-                            } else {
-                              return (
-                                <TableCell key={column.id} align={column.align}>
-                                  {column.format && typeof value === 'number'
-                                    ? column.format(value)
-                                    : value}
-                                </TableCell>
-                              );
-                            }
-                          })}
-                        </TableRow>
-                      );
-                    })
-                )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        {props.data == null ? null : (
-          <TablePagination
-            rowsPerPageOptions={[6, 36]}
-            component="div"
-            count={props.data.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        )}
-      </Paper> */}
     </Page>
   );
 }
