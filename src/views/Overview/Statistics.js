35b3988c2f7d4e11dcc1c198038f3fbd1ab56a4c
pennert <pennert@uw.edu>
2020-09-11 15:31:26 -0700

    Added counts to statistics on Overview

--- src/views/Overview/Statistics.js
+++ src/views/Overview/Statistics.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
 import { Card, Typography, Grid } from '@material-ui/core';
+import firebase from '../../firebase/firebase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,10 +26,20 @@ function Statistics({ className, ...rest }) {
   const classes = useStyles();
 
   const [totalProjects, setTotalProjects] = useState();
-  const [totalTasks, setTotalTasks] = useState();
   const [totalUsers, setTotalUsers] = useState();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function getTotalProjects() {
+      const snapshot = await firebase.db.collection('projects').get();
+      setTotalProjects(snapshot.size);
+    }
+    async function getTotalUsers() {
+      const snapshot = await firebase.db.collection('users').get();
+      setTotalUsers(snapshot.size);
+    }
+    getTotalProjects();
+    getTotalUsers();
+  }, []);
 
   return (
     <Grid container spacing={3} className={classes.grid}>
@@ -43,21 +54,6 @@ function Statistics({ className, ...rest }) {
             </div>
             <Typography component="h6" variant="overline">
               projects
-            </Typography>
-          </div>
-        </Card>
-      </Grid>
-      <Grid item lg={3} sm={6} xs={12}>
-        <Card {...rest} className={clsx(classes.subroot, className)}>
-          <div>
-            <Typography component="h3" gutterBottom variant="overline">
-              Total Tasks
-            </Typography>
-            <div className={classes.details}>
-              <Typography variant="h3">{totalTasks}</Typography>
-            </div>
-            <Typography component="h6" variant="overline">
-              tasks
             </Typography>
           </div>
         </Card>
