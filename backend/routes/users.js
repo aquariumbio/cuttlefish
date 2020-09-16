b87c906c61a3650177ab905c695325170ccbd6e2
pennert <pennert@uw.edu>
2020-09-15 17:32:30 -0700

    Added instruction for using different instances of Aquarium

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,7 @@ const AQ = require('../aquarium');
 
 // Login to aquarium
 router.post('/login', function(req, res, next) {
+  console.log(args);
   AQ.login(req.body.username, req.body.password)
     .then(data => res.status(200).send(data))
     .catch(err => res.status(400).send(err));
